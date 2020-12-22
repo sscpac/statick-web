7161e37d0a360c2a54285ff9221a5f8d480d2a46
Alexander Xydes <alexander.xydes@navy.mil>
2020-12-22 14:42:56 -0800

    Using new find_files function from DiscoveryPlugin which only walks the path once.
    Signed-off-by: Alexander Xydes <alexander.xydes@navy.mil>

--- src/statick_web/plugins/web_discovery_plugins/html_discovery_plugin.py
+++ src/statick_web/plugins/web_discovery_plugins/html_discovery_plugin.py
@@ -2,7 +2,6 @@
 
 from __future__ import print_function
 
-import fnmatch
 import os
 import subprocess
 from collections import OrderedDict
@@ -20,42 +19,22 @@ class HTMLDiscoveryPlugin(DiscoveryPlugin):
     def scan(self, package, level, exceptions=None):
         """Scan package looking for HTML files."""
         src_files = []
-        globs = ["*.html"]
 
-        file_cmd_exists = True
-        if not DiscoveryPlugin.file_command_exists():
-            file_cmd_exists = False
+        self.find_files(package)
 
-        root = ""
-        files = []
-        for root, _, files in os.walk(package.path):
-            for glob in globs:
-                for f in fnmatch.filter(files, glob):
-                    full_path = os.path.join(root, f)
-                    src_files.append(os.path.abspath(full_path))
-
-            if file_cmd_exists:
-                for f in files:
-                    full_path = os.path.join(root, f)
-                    output = subprocess.check_output(
-                        ["file", full_path], universal_newlines=True
-                    )
-                    # pylint: disable=unsupported-membership-test
-                    if "HTML document" in output:
-                        # pylint: enable=unsupported-membership-test
-                        src_files.append(os.path.abspath(full_path))
+        for file_dict in package.files.values():
+            if file_dict["name"].endswith(".html") or "html document" in file_dict["file_cmd_out"]:
+                src_files.append(file_dict["path"])
 
         src_files = list(OrderedDict.fromkeys(src_files))
 
         print("  {} HTML source files found.".format(len(src_files)))
         if exceptions:
             original_file_count = len(src_files)
-            src_files = exceptions.filter_file_exceptions_early(package, src_files)
+            src_files = exceptions.filter_file_exceptions_early(package,
+                                                                src_files)
             if original_file_count > len(src_files):
-                print(
-                    "  After filtering, {} HTML files will be scanned.".format(
-                        len(src_files)
-                    )
-                )
+                print("  After filtering, {} HTML files will be scanned.".
+                      format(len(src_files)))
 
         package["html_src"] = src_files
