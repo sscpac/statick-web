b95ba5742f0a428db711d0468d3181d361a93025
Alexander Xydes <alexander.l.xydes.civ@us.navy.mil>
2024-11-08 16:12:49 -0800

    Adding configs to extend from

--- tests/tool/eslint_tool_plugin/valid_package/rsc/eslint.config.mjs
+++ tests/tool/eslint_tool_plugin/valid_package/rsc/eslint.config.mjs
@@ -1,5 +1,9 @@
 import html from "eslint-plugin-html"
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier"
 export default [
+    js.configs.recommended,
+    eslintConfigPrettier,
     {
         files: ["**/*.html"],
         plugins: { html },
